47164edfaf9ec2d0fe8025b57d05ae0e7fad87e1
yyamada12 <12yacropolisy@gmail.com>
2019-05-10 08:02:02 +0900

    [add] Define Your Data Model

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -1,34 +1,34 @@
-//
-//  FoodTrackerTests.swift
-//  FoodTrackerTests
-//
-//  Created by 山田　祥允 on 2019/05/09.
-//  Copyright © 2019 yyamada. All rights reserved.
-//
-
 import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // MARK: Meal Class Tests
+    func testMealInitializationSucceeds() {
+        
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    // Confirm that the Meal initializer returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
     }
 
 }
